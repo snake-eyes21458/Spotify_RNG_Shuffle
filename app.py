7a74b7239677c90e194daac4fe44751e32fad0ae
snake-eyes21458 <135985631+snake-eyes21458@users.noreply.github.com>
2025-06-30 13:20:59 -0400

    Update app.py

--- app.py
+++ app.py
@@ -167,7 +167,7 @@ if __name__ == "__main__":
     port = int(os.environ.get("PORT", 7860))
     demo.launch(
         server_name="0.0.0.0",
-        port=port,
+        server_port=port,
         share=False
     )
 
